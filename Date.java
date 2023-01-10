8d646755ab9da7c78352bc1c44353d68b550aa6a
Frazman11 <116517981+Frazman11@users.noreply.github.com>
2023-01-10 09:25:53 -0800

    Create Date.java

---
+++ Date.java
@@ -0,0 +1,12 @@
+public class Date
+{
+    public static void main(String[] args) {
+        String day = "Tuesday";
+        String date = "10th";
+        String month = " January ";
+        int year = 2023;
+        
+        System.out.println("American Format: " + day + "," + month + date + ", " + year);
+        System.out.println("European Format: " + day + month + date + " " + year);
+    }
+}
